db130c2f63ed8786b00b6754c9b3ed2e5cb363a9
Igor Olkhov <smartestbox@gmail.com>
2024-01-09 20:49:01 +0100

    temp commit

--- src/shared/ui/radio-group/radio-group.tsx
+++ src/shared/ui/radio-group/radio-group.tsx
@@ -1,49 +1,25 @@
 import * as RadioGroupPrimitive from '@radix-ui/react-radio-group'
 import s from './radio-group.module.scss'
 
+const RadioGroupElement = (props) => {
+    const {title} = props
+    return (
+        <div className={s.RadioGroupItem}>
+            <RadioGroupPrimitive.Item className={s.RadioGroupCheckbox} value="default" id="r1">
+                <RadioGroupPrimitive.Indicator className={s.RadioGroupIndicator}/>
+            </RadioGroupPrimitive.Item>
+            <label className="Label" htmlFor="r1">
+                {title}
+            </label>
+        </div>
+    )
+}
+
 export function RadioGroup() {
     return (
         <RadioGroupPrimitive.Root className={s.RadioGroupRoot} defaultValue="default" aria-label="View density">
-            <div style={{display: 'flex', alignItems: 'center'}}>
-                <RadioGroupPrimitive.Item className={s.RadioGroupItem} value="default" id="r1">
-                    <RadioGroupPrimitive.Indicator className={s.RadioGroupIndicator}/>
-                </RadioGroupPrimitive.Item>
-                <label className="Label" htmlFor="r1">
-                    RadioGroup
-                </label>
-            </div>
-            <div style={{display: 'flex', alignItems: 'center'}}>
-                <RadioGroupPrimitive.Item className={s.RadioGroupItem} value="default" id="r1">
-                    <RadioGroupPrimitive.Indicator className={s.RadioGroupIndicator}/>
-                </RadioGroupPrimitive.Item>
-                <label className="Label" htmlFor="r1">
-                    RadioGroup
-                </label>
-            </div>
-            <div style={{display: 'flex', alignItems: 'center'}}>
-                <RadioGroupPrimitive.Item className={s.RadioGroupItem} value="default" id="r1">
-                    <RadioGroupPrimitive.Indicator className={s.RadioGroupIndicator}/>
-                </RadioGroupPrimitive.Item>
-                <label className="Label" htmlFor="r1">
-                    RadioGroup
-                </label>
-            </div>
-            <div style={{display: 'flex', alignItems: 'center'}}>
-                <RadioGroupPrimitive.Item className={s.RadioGroupItem} value="default" id="r1">
-                    <RadioGroupPrimitive.Indicator className={s.RadioGroupIndicator}/>
-                </RadioGroupPrimitive.Item>
-                <label className="Label" htmlFor="r1">
-                    RadioGroup
-                </label>
-            </div>
-            <div style={{display: 'flex', alignItems: 'center'}}>
-                <RadioGroupPrimitive.Item className={s.RadioGroupItem} value="default" id="r1">
-                    <RadioGroupPrimitive.Indicator className={s.RadioGroupIndicator}/>
-                </RadioGroupPrimitive.Item>
-                <label className="Label" htmlFor="r1">
-                    RadioGroup
-                </label>
-            </div>
+           <RadioGroupElement title={'RadioGroup'}/>
         </RadioGroupPrimitive.Root>
     )
 }
+
