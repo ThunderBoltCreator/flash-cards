5ed82ab1db71c487e7cb05dd136d40fa5ab06d6a
Stepan Shitikov <stepan.shchitikov.work@gmail.com>
2024-01-10 15:19:02 +0300

    tabs and card-switcher init

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Button } from 'shared/ui/button/button'
 import { Header } from 'shared/ui/header/header'
 import { Slider } from 'shared/ui/slider'
 import { TextFields } from 'shared/ui/text-field'
+import { CardSwitcher } from 'widgets/card-switcher/ui/card-switcher'
 
 export function App() {
   return (
@@ -13,6 +14,7 @@ export function App() {
       <TextFields.Password placeholder={'sdasds'} />
       <TextFields.BaseField placeholder={'sdasds'} />
       <Slider max={100} min={10} />
+      <CardSwitcher />
     </div>
   )
 }
