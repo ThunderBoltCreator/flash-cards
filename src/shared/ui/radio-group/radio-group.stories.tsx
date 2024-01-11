ec643cab1b8a973136b6b762e3c9b9f487968106
Igor Olkhov <smartestbox@gmail.com>
2024-01-11 17:55:24 +0100

    Add radioGroup radix component

--- src/shared/ui/radio-group/radio-group.stories.tsx
+++ src/shared/ui/radio-group/radio-group.stories.tsx
@@ -1,26 +1,38 @@
-import type {Meta, StoryObj} from '@storybook/react'
-import {RadioGroup} from "./";
+import type { Meta, StoryObj } from '@storybook/react'
 
+import { RadioGroup } from './'
 
 const meta = {
-    component: RadioGroup,
-    tags: ['autodocs'],
-    title: 'Components/Radio Group',
+  argTypes: {},
+  component: RadioGroup,
+  tags: ['autodocs'],
+  title: 'Components/Radio Group',
 } satisfies Meta<typeof RadioGroup>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
-    args: {
-        variant: 'primary',
-    },
+  args: {
+    options: [
+      { label: 'Very bad', value: 'very bad' },
+      { label: 'Bad', value: 'bad' },
+      { label: 'Not so bad', value: 'not so bad' },
+      { label: 'Good', value: 'good' },
+      { label: 'Perfect', value: 'perfect' },
+    ],
+  },
 }
 
 export const Disabled: Story = {
-    args: {
-        disabled: true,
-        variant: 'primary',
-    },
+  args: {
+    disabled: true,
+    options: [
+      { label: 'Very bad', value: 'very bad' },
+      { label: 'Bad', value: 'bad' },
+      { label: 'Not so bad', value: 'not so bad' },
+      { label: 'Good', value: 'good' },
+      { label: 'Perfect', value: 'perfect' },
+    ],
+  },
 }
-
--- src/shared/ui/radio-group/radio-group.tsx
+++ src/shared/ui/radio-group/radio-group.tsx
@@ -1,25 +1,40 @@
+import type { ComponentPropsWithoutRef } from 'react'
+
 import * as RadioGroupPrimitive from '@radix-ui/react-radio-group'
+
 import s from './radio-group.module.scss'
 
-const RadioGroupElement = (props) => {
-    const {title} = props
-    return (
-        <div className={s.RadioGroupItem}>
-            <RadioGroupPrimitive.Item className={s.RadioGroupCheckbox} value="default" id="r1">
-                <RadioGroupPrimitive.Indicator className={s.RadioGroupIndicator}/>
-            </RadioGroupPrimitive.Item>
-            <label className="Label" htmlFor="r1">
-                {title}
-            </label>
-        </div>
-    )
+type RadioRootProps = ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Root> & {
+  options: Option[]
+}
+type RadioElementProps = ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Item> & {
+  label: string
 }
 
-export function RadioGroup() {
-    return (
-        <RadioGroupPrimitive.Root className={s.RadioGroupRoot} defaultValue="default" aria-label="View density">
-           <RadioGroupElement title={'RadioGroup'}/>
-        </RadioGroupPrimitive.Root>
-    )
+type Option = {
+  label: string
+  value: string
 }
 
+export function RadioGroup({ options, ...props }: RadioRootProps) {
+  return (
+    <RadioGroupPrimitive.Root className={s.radioGroupRoot} {...props}>
+      {options.map(option => (
+        <RadioGroupElement key={option.value} label={option.label} value={option.value} />
+      ))}
+    </RadioGroupPrimitive.Root>
+  )
+}
+
+const RadioGroupElement = (props: RadioElementProps) => {
+  const { label } = props
+
+  return (
+    <div className={s.radioGroupElement}>
+      <RadioGroupPrimitive.Item className={s.radioGroupCheckbox} value={'default'}>
+        <RadioGroupPrimitive.Indicator className={s.radioGroupIndicator} />
+      </RadioGroupPrimitive.Item>
+      <label className={s.label}>{label}</label>
+    </div>
+  )
+}
